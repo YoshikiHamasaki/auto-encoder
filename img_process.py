34fbf25edc3abfdcbd965d095e9d9779f7cec77b
sakamotoyuta <Yuta.Sakamoto@mg4.seikyou.ne.jp>
2020-02-24 17:32:10 +0900

    二値化のやつ関数化

--- img_process.py
+++ img_process.py
@@ -1,12 +1,12 @@
 import cv2
-
+import glob
 
 image_number = 16
-
-
-for i in range(image_number):
-    img = cv2.imread("../image-data/AE_test_good/" + "{0:03d}.jpg".format(i), cv2.IMREAD_GRAYSCALE)
-    ret, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
-
-    cv2.imwrite("../image-data/AE_test_good_bin/" + "{0:03d}.jpg".format(i),img)
+def Binariz(input_path, output_path):
+    files = glob.glob(input_path + '/*')
+    for i, f in enumerate(files):
+        img = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
+        ret, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
     
+        cv2.imwrite(output_path + "{0:03d}.jpg".format(i),img)
+        
