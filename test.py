b5afa80c328d3f3d4fbf5911066a43707f41eae6
YoshikiHamasaki <hamasaki.yoshiki731@mail.kyutech.jp>
2020-08-26 16:54:08 +0900

    HSVテスト

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import torchvision
 import matplotlib.pyplot as plt
 
 
-train_loader, test_loader = dataset.color_make_data_set("csv/train.csv","csv/test_bad.csv","../image-data/AE_train","../image-data/AE_test_bad")
+train_loader, test_loader = dataset.color_make_data_set("csv/train.csv","csv/test_bad.csv","../image-data/train","../image-data/test_bad")
 
 def imshow(img):
     img = torchvision.utils.make_grid(img)
@@ -19,4 +19,18 @@ def imshow(img):
 
 iterator = iter(train_loader)
 img, _ = next(iterator)
-imshow(img[2])
+#imshow(img[2])
+
+a = cv2.imread("C:/Users/admin.H115/git/image-data/8_12_train/166.jpg")
+hsv = cv2.cvtColor(a,cv2.COLOR_BGR2HSV_FULL)
+
+plt.imshow(hsv)
+#plt.show()
+print(hsv[:,:,0])
+a = cv2.imread("C:/Users/admin.H115/git/image-data/test_shadow/171.jpg")
+hsv2 = cv2.cvtColor(a,cv2.COLOR_BGR2HSV_FULL)
+
+plt.imshow(hsv2)
+#plt.show()
+print(hsv2[:,:,0])
+
