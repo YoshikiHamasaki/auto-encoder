b6d4c011889d2e991cbc8e64415259dbdce53f61
YoshikiHamasaki <hamasaki.yoshiki731@mail.kyutech.jp>
2020-09-02 15:09:56 +0900

    画像解析

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 
+test_path = "C:/Users/admin.H115/git/image-data/test_shadow/171.jpg" 
+
+
+
+
 
 train_loader, test_loader = dataset.color_make_data_set("csv/train.csv","csv/test_bad.csv","../image-data/train","../image-data/test_bad")
 
@@ -22,17 +27,21 @@ img, _ = next(iterator)
 #imshow(img[2])
 
 a = cv2.imread("C:/Users/admin.H115/git/image-data/8_12_train/166.jpg")
-hsv = cv2.cvtColor(a,cv2.COLOR_BGR2HSV_FULL)
+image = cv2.cvtColor(a,cv2.COLOR_BGR2HSV_FULL)
 
-#plt.imshow(hsv)
-#plt.show()
-print(hsv[:,:,2])
-b = cv2.imread("C:/Users/admin.H115/git/image-data/test_shadow/104.jpg")
-hsv2 = cv2.cvtColor(b,cv2.COLOR_BGR2HSV_FULL)
+#print(hsv[:,:,2])
+b = cv2.imread(test_path)
+hsv1 = cv2.cvtColor(b,cv2.COLOR_BGR2HSV_FULL)
+b = cv2.cvtColor(b,cv2.COLOR_BGR2RGB)
 
-#plt.imshow(hsv2)
-#plt.show()
-print(hsv2[:,:,2])
+plt.figure(figsize=(9,7))
+plt.subplot(2,1,1)
+plt.imshow(b)
+plt.subplot(2,1,2)
+plt.imshow(hsv1)
+plt.show()
+
+#print(hsv2[:,:,2])
 
 #c = cv2.imread("C:/Users/admin.H115/git/image-data/8_12_train/166.jpg")
 #lab = cv2.cvtColor(c,cv2.COLOR_BGR2LAB)
