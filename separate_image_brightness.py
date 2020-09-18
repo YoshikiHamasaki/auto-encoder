d3ab00fc0eaa6c4b827c398265a48fd107695a62
YoshikiHamasaki <hamasaki.yoshiki731@mail.kyutech.jp>
2020-09-18 16:10:49 +0900

    明度分類

--- separate_image_brightness.py
+++ separate_image_brightness.py
@@ -4,20 +4,24 @@ import cv2
 import numpy as np
 
 test_path = "C:/Users/admin.H115/git/image-data/train_shadow_bright_about100" 
-csv_name = "csv/analusis/train_shadow_bright_about100_param.csv"
+csv_name = "csv/analysis/train_shadow_bright_about100_param.csv"
 
 img_name = os.listdir(test_path) 
 
 
+abusolute_dif = []
+ave = []
 for i in range(1):
 
     image = cv2.imread(os.path.join(test_path, img_name[i]))
     image = cv2.cvtColor(image,cv2.COLOR_BGR2LAB)
     image_lab = image[:,:,0]
-    image_lab.reshape(1,28*28)
-    print(image_lab.shape)
+    abusolute_dif.append(np.max(image_lab)-np.min(image_lab))
+    ave.append
     print(np.min(image_lab))
-    df = pd.DataFrame()
+
+
+#df = pd.DataFrame()
     #df = df.set_index("num")
     #df.to_csv(csv_name, mode ="a",header = None)
 
