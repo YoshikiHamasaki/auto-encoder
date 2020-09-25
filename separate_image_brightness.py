e01e26cce7a973d73e6243565e7c9b926bba53ef
YoshikiHamasaki <hamasaki.yoshiki731@mail.kyutech.jp>
2020-09-25 16:29:02 +0900

    csv出力

--- separate_image_brightness.py
+++ separate_image_brightness.py
@@ -2,27 +2,39 @@ import pandas as pd
 import os
 import cv2
 import numpy as np
+import scipy.stats as stats
+import math
 
-test_path = "C:/Users/admin.H115/git/image-data/train_shadow_bright_about100" 
-csv_name = "csv/analysis/train_shadow_bright_about100_param.csv"
+test_path = "C:/Users/admin.H115/git/image-data/test_shadow_bad" 
+csv_name = "csv/analysis/test_shadow_param.csv"
 
 img_name = os.listdir(test_path) 
 
 
 abusolute_dif = []
 ave = []
-for i in range(1):
+mode = []
+list_data = []
+
+
+for i in range(len(img_name)):
 
     image = cv2.imread(os.path.join(test_path, img_name[i]))
     image = cv2.cvtColor(image,cv2.COLOR_BGR2LAB)
     image_lab = image[:,:,0]
     abusolute_dif.append(np.max(image_lab)-np.min(image_lab))
-    ave.append
-    print(np.min(image_lab))
+    ave.append(np.average(image_lab))
+    uniqs,counts = np.unique(image_lab,return_counts=True)
+    mode.append(str(uniqs[counts == np.amax(counts)]).strip("[]"))
+
+list_data = [abusolute_dif,
+            ave,
+            mode]
 
 
-#df = pd.DataFrame()
-    #df = df.set_index("num")
-    #df.to_csv(csv_name, mode ="a",header = None)
+df = pd.DataFrame(data = list_data)
+df_T = df.T
+df_T.columns = ["abusolute_diff","average","mode"]
+df_T.to_csv(csv_name, mode ="w")
 
 
